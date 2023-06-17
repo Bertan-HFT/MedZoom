f968ffbe5b47b4ca590bbd3b01afeea181c014b0
BertanDincer <02dibe1bwi@hft-stuttgart.de>
2023-06-17 21:44:25 +0200

    Final fix

--- lib/pages/neue_fahrt_planen/neue_fahrt_planen_widget.dart
+++ lib/pages/neue_fahrt_planen/neue_fahrt_planen_widget.dart
@@ -383,7 +383,7 @@ class _NeueFahrtPlanenWidgetState extends State<NeueFahrtPlanenWidget> {
                         ),
                       ],
                       rows: List.generate(
-                        _patients.length ~/ 2,
+                        _patients.length,
                         (index) => [
                           Text(
                             _patients[index],
